2fc7c520657d71a1eadcd82e8368f8d340a89704
BenC <benjamin.f.clardy.v@gmail.com>
2025-02-06 10:56:12 -0800

    First upload with helloworld style function as proof of concept.

---
+++ R/hello.R
@@ -0,0 +1,10 @@
+#' Say Hello from PackaBen
+#'
+#' This function prints a friendly greeting.
+#'
+#' @return A character string with the greeting.
+#' @export
+hello <- function() {
+  message("Hello from PackaBen!")
+  return("Hello from PackaBen!")
+}
